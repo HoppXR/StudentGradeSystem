f3f422ac1eff1170cedba0433cb95e407292e826
HoppXR <brtl.hopper@gmail.com>
2024-01-22 20:04:02 -0500

    Initial commit

---
+++ StudentGradeSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGradeSystem
+{
+    class StudentGradeSystem
+    {
+        private List<Student> students = new List<Student>();
+
+        public StudentGradeSystem()
+        {
+            students.Add(new Student("Joe"));
+        }
+
+        public void CheckGradeSystem()
+        {
+
+        }
+        
+        public void AddStudent()
+        {
+
+        }
+
+        public void RemoveStudent()
+        {
+
+        }
+    }
+
+    class Student
+    {
+        public Student(string name)
+        {
+
+        }
+    }
+
+    class Grade
+    {
+
+    }
+
+    class Course
+    {
+
+    }
+}
